d73030c1a9a11838e2209a8f0a9ad9578207f47f
Rashid Ali <rashid.ali@teo-intl.com>
2023-10-10 08:45:30 +0200

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public List<Employee> Employees = new List<Employee> 
+        { 
+          new Employee { Id = 1,Name="Ali"},
+          new Employee { Id = 2,Name="Kamran"}
+        };
+
+        [HttpGet("GetAllEmployees")]
+        public IActionResult GetAll()
+        {
+            return Ok(Employees);
+        }
+        [HttpGet("Get/{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var emp= Employees.FirstOrDefault(x => x.Id == id);
+            if(emp == null)
+            {
+                return NotFound("employee not found");
+            }
+            return Ok(emp);
+        }
+
+        [HttpPost("AddEmployee")]
+        public IActionResult Add(Employee employee)
+        {
+            Employees.Add(employee);
+            return Created("",Employees);
+        }
+        [HttpPut("UpdateEmployee")]
+        public IActionResult Update(Employee employee)
+        {
+            var emp = Employees.FirstOrDefault(x => x.Id == employee.Id);
+            if(emp != null)
+            {
+                emp.Name = employee.Name;
+                emp.City = employee.City;
+                emp.Address = employee.Address;
+                emp.Region=employee.Region;
+            }
+            else
+            {
+                return NotFound("employee not found");
+            }
+            return Ok(Employees);
+        }
+
+        [HttpDelete("Delete/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var emp = Employees.FirstOrDefault(x => x.Id == id);
+            if(emp != null)
+            {
+                Employees.Remove(emp);
+            }
+            else
+            {
+                return NotFound("employee not found");
+            }
+            return Ok(Employees);
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public string Address { get; set; } = default!;
+
+        public string City { get; set; } = default!;
+
+        public string Region { get; set; } = default!;
+    }
+}
